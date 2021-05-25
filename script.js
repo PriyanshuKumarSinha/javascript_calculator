addf88c53d5714bb6debf17af6cd4a22adbf4980
PriyanshuKumarSinha <priyanshuk9066@gmail.com>
2021-05-25 23:36:34 +0530

    added some functionalities and fixed bugs

--- script.js
+++ script.js
@@ -12,18 +12,19 @@ window.addEventListener('resize',
     }
 )
 
-var calcKeys= [7,8,9,"+",4,5,6,'-',1,2,3,'*',0,'.','/','=']
+var calcKeys= [7,8,9,"+",4,5,6,'-',1,2,3,'*',0,'.','/','=', '(',')']
 
 for (let i=0; i < calcKeys.length; i++ ){
     createButtons('button1',calcKeys[i]);
 }
 
-function createButtons(className,value = ''){
+createButtons('button1','√')
+function createButtons(className,value = '', func = 'fun(this.id)' ){
     var button = document.createElement('button');
 
     button.setAttribute('class',`btn btn-secondary ${className}`);
     button.setAttribute('id', `${value}`);
-    button.setAttribute('onclick','fun(this.id)');
+    button.setAttribute('onclick',func);
     
     button.innerHTML = `<b> .<span>${value}</span>. </b>`;
 
@@ -35,11 +36,10 @@ function createButtons(className,value = ''){
     calcBody.appendChild(button);
 
 }
-
 function clearButton(){
     var button = document.createElement('button');
 
-    button.setAttribute('class',`btn btn-danger btn-lg`);
+    button.setAttribute('class',`btn btn-danger `);
     button.setAttribute('onclick','clearScreen()');
     button.style.margin = '10px';
     
@@ -52,14 +52,36 @@ clearButton();
 
 var screen = document.querySelector('.calc-screen');
 
+var previousOperator;
 function fun(buttonValue){
+
     if (buttonValue === '='){
-        screen.innerHTML = eval(screen.innerHTML)
+        if(isFloat(eval(screen.innerHTML))){
+            screen.innerHTML = eval(screen.innerHTML).toFixed(2)
+        }
+        else{
+            screen.innerHTML = eval(screen.innerHTML)
+        }
         screen.textContent.bold;
+        previousOperator = "=";
+    }
+    else if(buttonValue === '√'){
+        previousOperator = "";
+        screen.innerHTML += 'Math.sqrt('
+    }
+    else if(Number.isInteger(parseInt(buttonValue)) && previousOperator === '='){
+        clearScreen();
+        screen.innerHTML += buttonValue;
+
     }
     else{
+        previousOperator = "";
         screen.innerHTML += buttonValue;
     }
+}
+
+function isFloat(n){
+    return Number(n) === n && n % 1 !== 0;
 }
 
 function clearScreen(){
